e4c5aeb063b45c5c714984a0d8573cf7032d8a67
Junho Yeo <i@junho.io>
2022-01-17 04:40:05 +0900

    Implement custom hook `useImmutableXBalance`

---
+++ src/hooks/useImmutableXBalance.ts
@@ -0,0 +1,41 @@
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+
+import { ImmutableXClient } from '@imtbl/imx-sdk';
+
+type UseBalanceParams = {
+  client: ImmutableXClient;
+  address: string;
+};
+
+type ImmutableXBalance = {
+  imx: ethers.BigNumber;
+  preparingWithdrawal: ethers.BigNumber;
+  withdrawable: ethers.BigNumber;
+};
+
+export const useImmutableXBalance = ({
+  client,
+  address,
+}: UseBalanceParams): ImmutableXBalance | null => {
+  const [balance, setBalance] = useState<ImmutableXBalance | null>(null);
+
+  useEffect(() => {
+    if (!client || !address) {
+      return;
+    }
+    client
+      .getBalances({
+        user: address as any, // unbranding from `EthAddressBrand`
+      })
+      .then((data) => {
+        setBalance({
+          imx: data.imx,
+          preparingWithdrawal: data.preparing_withdrawal,
+          withdrawable: data.withdrawable,
+        });
+      });
+  }, [client, address]);
+
+  return balance;
+};
