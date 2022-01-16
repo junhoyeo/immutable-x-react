8d854b5f458ae111ba4b51b643c61ddc10b6c9a8
Junho Yeo <i@junho.io>
2022-01-17 03:59:12 +0900

    Add call to Setup wallet

--- src/Landing/LandingPage.tsx
+++ src/Landing/LandingPage.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
 
+import { useImmutableX } from '@/hooks/useImmutableX';
+
 const LandingPage = () => {
-  return <Container />;
+  const { client, link } = useImmutableX('mainnet');
+
+  const onClickSetupIMX = useCallback(async () => {
+    const { address, starkPublicKey } = await link.setup({});
+    console.log(address, starkPublicKey);
+  }, [link]);
+
+  return (
+    <Container>
+      <SetupButton onClick={onClickSetupIMX}>Setup IMX</SetupButton>
+    </Container>
+  );
 };
 
 export default LandingPage;
 
 const Container = styled.div``;
+
+const SetupButton = styled.button``;
