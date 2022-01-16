02d0f3d434596753324d14a0c598d79c699062be
Junho Yeo <i@junho.io>
2022-01-17 05:56:50 +0900

    Implement custom hook `useImmutableXAssets`

---
+++ src/hooks/useImmutableXAssets.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { ImmutableMethodResults, ImmutableXClient } from '@imtbl/imx-sdk';
+
+type UseAssetsParams = {
+  client: ImmutableXClient;
+  address: string;
+};
+
+export type ImmutableXAsset = ImmutableMethodResults.ImmutableAsset;
+
+export const useImmutableXAssets = ({ client, address }: UseAssetsParams) => {
+  const [assets, setAssets] = useState<ImmutableXAsset[]>([]);
+  const [total, setTotal] = useState<number>(0);
+
+  const [currentCursor, setCurrentCursor] = useState<string | undefined>(
+    undefined,
+  );
+  const [nextCursor, setNextCursor] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    client
+      .getAssets({
+        user: address,
+        cursor: currentCursor,
+      })
+      .then((data) => {
+        setAssets((prev) => [...prev, ...data.result]);
+        setTotal(data.remaining + data.result.length);
+        if (data.cursor) {
+          setNextCursor(data.cursor);
+        }
+      });
+  }, [client, address, currentCursor]);
+
+  const fetchMore = useCallback(() => {
+    setCurrentCursor(nextCursor);
+    setNextCursor(undefined);
+  }, [nextCursor]);
+
+  return { assets, total, currentCursor, nextCursor, fetchMore };
+};
