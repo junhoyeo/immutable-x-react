ab786a6964f8ce3c323e22f5eba8adfe330c5e67
Junho Yeo <i@junho.io>
2022-01-17 04:28:57 +0900

    Dynamically import `LandingPage` without `ssr`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,7 @@
-export { default } from '@/Landing/LandingPage';
+import dynamic from 'next/dynamic';
+
+const DynamicPage = dynamic(() => import('@/Landing/LandingPage'), {
+  ssr: false,
+});
+
+export default DynamicPage;
